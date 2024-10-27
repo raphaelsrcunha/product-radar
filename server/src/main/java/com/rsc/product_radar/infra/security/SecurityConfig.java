c936103d05dafba55f2f93561028b74e5859e23a
Raphael Cunha <raphaelsrcunha@gmail.com>
2024-10-27 15:25:47 -0400

    remove unwanted comments

--- server/src/main/java/com/rsc/product_radar/infra/security/SecurityConfig.java
+++ server/src/main/java/com/rsc/product_radar/infra/security/SecurityConfig.java
@@ -37,7 +37,6 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        //.requestMatchers(HttpMethod.GET, "/products").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
